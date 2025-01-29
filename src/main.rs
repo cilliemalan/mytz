c2ff37a7be4cfa461ec03612abe6037da27fae00
Cillié Malan <cilliemalan@gmail.com>
2025-01-29 13:00:35 +0200

    Basic service outline

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
+use axum::{http::HeaderMap, routing::get, Router};
 
+async fn root_handler() -> &'static str {
+    "Hello, World!"
+}
 
-fn main() {
-    println!("Hello, world!");
+async fn tz_handler(headers: HeaderMap) -> String {
+    for (key, value) in headers.iter() {
+        println!("{}: {:?}", key, value);
+    }
+
+    let ip_addr = headers.get("X-Forwarded-For")
+        .map(|s| s.to_str().ok().unwrap_or(""))
+        .unwrap_or("");
+
+    format!("Hello, {ip_addr}!")
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root_handler))
+        .route("/tz", get(tz_handler));
+
+    let port = std::env::var("PORT").unwrap_or_else(|_| "3200".to_string());
+    let listen = format!("0.0.0.0:{}", port);
+    let listener = tokio::net::TcpListener::bind(listen).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
